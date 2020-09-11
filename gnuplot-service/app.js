84c5b4bbc81cdbd2ed50783ee3217aef08d86491
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-09-11 17:51:40 +0300

    commit

---
+++ gnuplot-service/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const body_parser = require("body-parser");
+const gnuplot = require("gnuplot");
+
+// Asenna paketit konsolista
+// npm install express
+// etc..
+
+// Alusta express sovellus
+const app = express();
+
+//Lisää parserit POST kutsun json muotoiselle bodylle
+//ja GET query parametreille
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({
+    extended: true
+}));
+
+//Käsittelijä POST kutsulle, jossa JSON muotoinen data
+//{
+//  "formula": "sin(x)+x**2"
+//}
+
+app.post("/", (req, res, next) => {
+    const formula = req.body.formula;
+
+    //plottaa png kuva
+    gnuplot()
+        .set("term png")
+        .unset("output")
+        .plot(formula, {
+            end: true
+        })
+        .pipe(res);
+
+    console.log(formula);
+    res.send("ok");
+});
+
+//Kuuntele porttia 80
+app.listen(8083);
